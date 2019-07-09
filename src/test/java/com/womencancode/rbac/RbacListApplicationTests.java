fc006899523ea33dbc74ef27ee0938eb6f6a5e31
Luana Pimentel <luana.pimentel85@gmail.com>
2019-07-09 16:40:34 -0300

    Changed application objective to a simple RBAC

--- src/test/java/com/womencancode/rbac/RbacListApplicationTests.java
+++ src/test/java/com/womencancode/rbac/RbacListApplicationTests.java
@@ -1,4 +1,4 @@
-package com.womencancode.todo;
+package com.womencancode.rbac;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ToDoListApplicationTests {
+public class RbacListApplicationTests {
 
     @Test
     public void contextLoads() {
