b1613a6b7dde50811aeca705bafbdcb90410061a
Luana Pimentel <luana.pimentel85@gmail.com>
2019-07-14 16:06:08 -0300

    Changed comparison method

--- src/test/java/com/womencancode/rbac/repository/ITRoleRepositoryTest.java
+++ src/test/java/com/womencancode/rbac/repository/ITRoleRepositoryTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
@@ -60,7 +61,7 @@ public class ITRoleRepositoryTest {
 
         //then
         Role expectedRole = mongoOperations.findById(id, Role.class);
-        assertThat(role, is(expectedRole));
+        assertEquals(expectedRole, role);
     }
 
     @Test
