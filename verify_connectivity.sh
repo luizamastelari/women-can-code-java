2d5f54d018fb9df95b5dda861bfb3c77be5ccd91
Luana Pimentel <luana.pimentel85@gmail.com>
2019-07-16 01:30:59 -0300

    Simplfied file

--- verify_connectivity.sh
+++ verify_connectivity.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
 # Does a /api/role call to the backend
-STATUS=$(curl -I -H "Content-Type: application/json" http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/role 2> /dev/null | head -n 1 | awk '{print $2}')
-
-if [ "$STATUS" = "200" ]
-then
-    echo "Application status OK"
-else
-    echo "Application failed to run"
-    exit 1
-fi
+curl -I -H "Content-Type: application/json" http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/role
